ba1a4c1eb92a8a7f72aaeee28d3aebd1709718a6
Eight Eight Studio <eighteightagency@gmail.com>
2026-03-23 16:27:39 +0530

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Image from 'next/image'
 import { ProductsClientPage } from './ProductsClientPage'
 import { CtaBanner } from '@/components/sections/CtaBanner'
 
